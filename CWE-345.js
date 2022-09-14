4abd7540d87d102ee520904f21b4e0f395a11bea
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 14:18:05 +0530

    Added referrer origin verification

--- CWE-345.js
+++ CWE-345.js
@@ -5,14 +5,21 @@ var fs = require('fs');
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+    var referer = req.headers.referer;
+    var host = req.headers.host;
+    if (referer && referer.indexOf(host) != -1) {
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end("404 Not Found");
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+    }
 }
 ).listen(8080);
