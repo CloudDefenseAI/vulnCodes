ec92199d793cedd1ba8ec5a64c44111ebe8fb264
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 12:59:13 +0530

    Added checks for path traversal attack

--- CWE-22.js
+++ CWE-22.js
@@ -2,10 +2,14 @@ var fs = require('fs');
 var path = require('path');
 
 var filePath = path.join(__dirname, req.query.file);
-fs.readFile(filePath, function(err, data) {
-    if (err) {
-        res.send('error');
-    } else {
-        res.send(data);
+if (filePath.indexOf(__dirname) === 0) {
+    fs.readFile(filePath, function(err, data) {
+        if (err) {
+            res.send('error');
+        } else {
+            res.send(data);
+        }
     }
+} else {
+    res.send('error');
 }
