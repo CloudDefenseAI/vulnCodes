c8ddabd528838185384d3f3ffab10a3233c63e26
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 17:05:30 +0530

    Fixed XSS

--- CWE-80.js
+++ CWE-80.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
 
+const XssSanitizer = require('xss-sanitizer');
+
 app.get('/save-profile', function(req, res) {
-    const name = req.query.name;
-    const age = req.query.age;
-    const email = req.query.email;
-    const phone = req.query.phone;
-    const address = req.query.address;
-    const city = req.query.city;
-    
+    const name = XssSanitizer.sanitize(req.query.name);
+    const age = XssSanitizer.sanitize(req.query.age);
+    const email = XssSanitizer.sanitize(req.query.email);
+    const phone = XssSanitizer.sanitize(req.query.phone);
+    const address = XssSanitizer.sanitize(req.query.address);
+    const city = XssSanitizer.sanitize(req.query.city);
+
     res.send(`Name: ${name} Age: ${age} Email: ${email} Phone: ${phone} Address: ${address} City: ${city}`);
 });
 
