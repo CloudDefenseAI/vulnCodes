38c7268f7f3d4790cfe5cb79d5c40c214225077f
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 17:13:02 +0530

    Fixed object Injection vulnerability

--- CWE-915.js
+++ CWE-915.js
@@ -5,9 +5,14 @@ var querystring = require('querystring');
 var server = http.createServer(function(req, res) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
+    const allowedKeys = ['foo', 'email']
     var obj = { foo: 'bar', csrfHash: 'EER3DS123DS4', email: 'jhon@example.com' };
-    var result = obj[query['key']];
-    res.end(result);
+    if (allowedKeys.includes(query['key'])) {
+        var result = obj[query['key']];
+        res.end(result);
+    }else {
+        res.end('Invalid key');
+    }
 });
 
 server.listen(8000);
