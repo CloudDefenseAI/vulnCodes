2ef4658b94bd6ac5d1a46d44e79722cb4a74ff15
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 16:57:28 +0530

    Create CWE-73.js

---
+++ CWE-73.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    var filename = req.query.filename;
+    var data = req.query.data;
+    fs.writeFile(path.join('/', filename), data, function(err) {
+        if (err) {
+            res.send('Error');
+        } else {
+            res.send('Success');
+        }
+    });
+})
+
+app.listen(3000);
