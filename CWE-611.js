a8d87635aeaa49a63b85b5c87211fb288e4723fa
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 16:52:42 +0530

    Create CWE-611.js

---
+++ CWE-611.js
@@ -0,0 +1,11 @@
+var xml = require('libxmljs');
+var fs = require('fs');
+var sys = require('sys');
+var app = require('express')();
+
+app.get('/user_input', function(req, res) {
+    var doc = xml.parseXmlString(req.query.user_input);
+    res.send(doc.toString());
+})
+
+app.listen(3000);
