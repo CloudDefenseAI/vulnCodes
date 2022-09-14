872fa695cc615c7b584483c910ddeea536cf423d
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 16:39:34 +0530

    Create CWE-522.js

---
+++ CWE-522.js
@@ -0,0 +1,15 @@
+app.post('/api/users/change-password', function(req, res) {
+    var userId = req.body.userId;
+    var password = req.body.password;
+    var user = app.models.user;
+    user.ChangePassword(userId, password, function(err, result) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send({
+                message: 'Password changed successfully',
+                result: result
+            });
+        }
+    });
+})
