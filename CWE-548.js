a362814972f23bf9024b467d3251be092052c171
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 16:47:16 +0530

    Update CWE-548.js

--- CWE-548.js
+++ CWE-548.js
@@ -1,20 +1,18 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
-    var ext = path.extname(filename);
-    var allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
-    if (allowedExtensions.indexOf(ext) > -1) {
-        fs.readFile(filename, function(err, data) {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end("404 Not Found");
-            }
-            res.writeHead(200, {'Content-Type': 'image/jpeg'});
-            res.write(data);
-            return res.end();
-        });
-    } else {
-        res.writeHead(403, {'Content-Type': 'text/html'});
-        return res.end("403 Forbidden");
-    }
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
