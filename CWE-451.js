cd0afcd790970f1fac8d1a185214620fe7b767f5
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-14 14:23:17 +0530

    added extension name with file name in UI

--- CWE-451.js
+++ CWE-451.js
@@ -17,7 +17,7 @@ modal.innerHTML = `
                 </button>
             </div>
             <div class="modal-body">
-                <p>Are you sure you want to download Camera_selfie image ?</p>
+                <p>Are you sure you want to download Camera_selfie.png image ?</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
