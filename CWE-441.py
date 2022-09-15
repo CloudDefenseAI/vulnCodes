a53f7cc03d15f009b2265a7288daf47ae42d5572
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-15 16:41:36 +0530

    fixed SSRF

--- CWE-441.py
+++ CWE-441.py
@@ -5,9 +5,13 @@ import json
 
 def main():
     url = sys.argv[1]
-    print(url)
-    response = requests.get(url)
-    print(response.text)
+    allowedDomains = ["https://www.website.com", "https://website.com", "https://internal.website.com"]
+    if url.startswith(tuple(allowedDomains)):
+        print(url)
+        response = requests.get(url)
+        print(response.text)
+    else:
+        print("Invalid URL")
 
 if __name__ == "__main__":
     main()
