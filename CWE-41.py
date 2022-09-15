e799de251a1903f26ca6f505392fd357bdeebb96
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-15 14:35:07 +0530

    Create CWE-41.py

---
+++ CWE-41.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+import os
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: %s <url> <file>" % sys.argv[0])
+        sys.exit(1)
+
+    url = sys.argv[1]
+    filename = sys.argv[2]
+
+    if not os.path.exists(filename):
+        print("File %s does not exist" % filename)
+        sys.exit(1)
+
+    with open(filename) as f:
+        data = f.read()
+
+    r = requests.post(url, data=data)
+    print(r.text)
+
+if __name__ == "__main__":
+    main()
