643da4ff301ab98c0dcb8850d45ce636b3030215
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-15 20:25:42 +0530

    Fixed OS Command injection

--- CWE-77.py
+++ CWE-77.py
@@ -1,8 +1,11 @@
-import requests, sys, os, json
+import requests, sys, os, json, re
 
 def main():
     filename = input("Enter the filename to open it: ")
     print("Opening file: " + filename)
+    if re.search(r'[^a-zA-Z0-9_]', filename):
+        print("[-] Invalid filename, please check the filename and try again")
+        sys.exit(1)
     output = os.popen("cat " + filename+".jpeg").read()
     print(output)
 
