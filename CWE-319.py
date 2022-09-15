5a7a9378764880c36f7417c602a2ddaa72987a7b
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-15 13:56:32 +0530

    Create CWE-319.py

---
+++ CWE-319.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import json
+import requests
+
+data = { "username": "admin", "password": "admin" }
+cookies = { "JSESSIONID": "1234567890" }
+headers = { "Content-Type": "application/json" }
+url = "http://website.com/login"
+
+def main():
+    r = requests.post(url, data=json.dumps(data), headers=headers, cookies=cookies, verify=False)
+    if r.status_code == 200:
+        print("Login successful")
+    else:
+        print("Login failed")
+
+if __name__ == "__main__":
+    main()
