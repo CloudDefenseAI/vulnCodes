e5843ee367e99c1f4fd5f1ab32bc008d4301f1f3
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-16 15:44:05 +0530

    Fixed buffer overflow

--- CWE-122.php
+++ CWE-122.php
@@ -1,6 +1,14 @@
-<?php 
+<?php
 $input = $_GET['input'];
 ini_set("memory_limit",-1);
+// let's protect against inputs like 0x7fffffff which will cause heap buffer overflow
+if(!is_numeric($input)) {
+    echo "Input is not numeric";
+    exit;
+}else if($input > 5000) {
+    echo "Input is too big";
+    exit;
+}
 $str=str_repeat("A", $input);
 $str.=$str;
 ?>
