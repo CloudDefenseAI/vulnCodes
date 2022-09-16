32b81ed1224f50bc44de09db2f04ea49b0589e89
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-16 15:58:01 +0530

    Fixed weak crypto hash

--- CWE-327.php
+++ CWE-327.php
@@ -1,6 +1,6 @@
 <?php
 $username = $_POST['username'];
 $password = $_POST['password'];
-$encrypted_password = base64_encode($password);
+$encrypted_password = password_hash($password, PASSWORD_DEFAULT);
 $sql = "INSERT INTO users (username, password) VALUES ('$username', '$encrypted_password')";
 ?>
