3cf6d12e7546376548b970ab183dd81e90b5930f
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-16 17:34:19 +0530

    added throttling

--- CWE-770.php
+++ CWE-770.php
@@ -1,11 +1,18 @@
 <?php
 $username = $_GET['username'];
 $password = $_GET['password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$throttle = new Throttle($username, $ip);
+if ($throttle->isThrottled()) {
+    echo "You are throttled";
+    die();
+}
 $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 $result = mysql_query($sql);
 if (mysql_num_rows($result) == 1) {
     echo "Login successful";
 } else {
+    $throttle->addAttempt();
     echo "Login failed";
 }
 ?>
