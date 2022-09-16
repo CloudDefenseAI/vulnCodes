389bd65957038308b59de0b89838d44853c6b82a
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-16 16:13:22 +0530

    Update CWE-497.php

--- CWE-497.php
+++ CWE-497.php
@@ -1,5 +1,10 @@
 <?php
 $userName = getCurrentUser();
-$command = 'ps aux | grep ' . $userName;
-system($command);
+// allow only if username is mike
+if($userName != "mike"){
+  echo "We can't show processes of other user";
+}else{
+  $command = 'ps aux | grep ' . $userName;
+  system($command);
+}
 ?>
