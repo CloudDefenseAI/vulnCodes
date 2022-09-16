71fda86b09b8af950a007752827b9c18e3f0f15e
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-16 17:16:24 +0530

    fixed command injection

--- CWE-77.php
+++ CWE-77.php
@@ -1,6 +1,12 @@
 <?php
 ## enter folder name to check whats inside that folder
 $folder = $_GET['folder'];
-$output = shell_exec('ls -la '.$folder);
-echo "<pre>$output</pre>";
+## check if folder exists any blank space or special character
+if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $folder))
+{
+    echo "Folder name is not valid";
+}else{
+    $output = shell_exec('ls -la '.$folder);
+    echo "<pre>$output</pre>";
+}
 ?>
