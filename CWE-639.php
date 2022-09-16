093bfa7be95a432a2ebbc22c0cfe66f89e6ef0c8
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-16 17:09:53 +0530

    Create CWE-639.php

---
+++ CWE-639.php
@@ -0,0 +1,9 @@
+<?php
+$userId = $_GET['user_id'];
+$query = "SELECT * FROM users WHERE id = $userId";
+$result = mysql_query($query);
+$row = mysql_fetch_array($result);
+echo "Your name is " . $row['name'];
+echo "Your email is " . $row['email'];
+echo "Your address is " . $row['address'];
+?>
