b0c282ab4fe8df6148e4b79aa984ed2512e107d7
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 14:20:30 +0530

    Fixed eval injection

--- CWE-95.java
+++ CWE-95.java
@@ -5,6 +5,8 @@ public class test {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        Runtime.getRuntime().exec("ping " + input);
+        if (input.matches("[a-zA-Z0-9.]+")) {
+            Runtime.getRuntime().exec("ping " + input);
+        }
     }
-} 
+}
