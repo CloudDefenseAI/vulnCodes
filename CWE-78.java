dffd228b0d6b2bf46f8bd148809024d2c75810e8
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 14:08:34 +0530

    Create CWE-78.java

---
+++ CWE-78.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your name, this will be used to create a directory");
+        String command = sc.nextLine();
+        Process p = Runtime.getRuntime().exec("mkdir " + command);
+        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = null;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
