2a17156cf29ab3e507af385da29b85716238a4b0
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 14:09:01 +0530

    Fixed Command INJECTION

--- CWE-78.java
+++ CWE-78.java
@@ -8,11 +8,16 @@ public class test {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your name, this will be used to create a directory");
         String command = sc.nextLine();
-        Process p = Runtime.getRuntime().exec("mkdir " + command);
-        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line = null;
-        while ((line = in.readLine()) != null) {
-            System.out.println(line);
+        // check if command contains any special characters then throw an exception
+        if (command.matches("^[a-zA-Z0-9]*$")) {
+            Process p = Runtime.getRuntime().exec("mkdir " + command);
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } else {
+            System.out.println("Invalid command");
         }
     }
 }
