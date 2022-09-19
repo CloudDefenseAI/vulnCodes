70c1fb08ab83e615923cb05af775845dfee2f476
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 11:51:34 +0530

    Create CWE-323.java

---
+++ CWE-323.java
@@ -0,0 +1,37 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        String key = "SUPER_SECRET_NONCE";
+        String data = "Hello World";
+        String encrypted = encrypt(key, data);
+        String decrypted = decrypt(key, encrypted);
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+    }
+
+    public static String encrypt(String key, String data) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decrypt(String key, String encrypted) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+        return new String(original);
+    }
+}
