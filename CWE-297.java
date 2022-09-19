c47038df7c4fabd0e0c14190d56a2a62ab69cbc4
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 11:44:14 +0530

    Fixed certificate validation issues

--- CWE-297.java
+++ CWE-297.java
@@ -6,11 +6,6 @@ public class test {
     public static void main(String[] args) throws Exception {
         URL url = new URL("https://www.google.com");
         HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-        con.setHostnameVerifier(new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) {
-                return true;
-            }
-        });
         con.setRequestMethod("GET");
         con.connect();
         System.out.println(con.getResponseCode());
