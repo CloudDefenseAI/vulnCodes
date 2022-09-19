6ce543ff055cabac2e40886d65a08176d8ff7108
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 14:01:23 +0530

    Create CWE-704.java

---
+++ CWE-704.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
+import java.util.function.*;
+
+public class test {
+    public static void main(String[] args) {
+        int i = 0;
+        i = (int) 0x80000000;
+        System.out.println(i);
+    }
+}
