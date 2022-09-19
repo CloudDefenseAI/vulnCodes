651c4767a2536cd2beabe2812585053301d8efb5
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 12:04:27 +0530

    Create CWE-329.java

---
+++ CWE-329.java
@@ -0,0 +1,26 @@
+import java.security.SecureRandom;
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
+import java.security.InvalidAlgorithmParameterException;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+
+public class SymmetricCipherTest {
+    public static void main() {
+        byte[] text ="Secret".getBytes();
+        byte[] iv ={
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+        };
+        KeyGenerator kg = KeyGenerator.getInstance("DES");
+        kg.init(56);
+        SecretKey key = kg.generateKey();
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        IvParameterSpec ips = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, key, ips);
+        return cipher.doFinal(inpBytes);
+    }
+}
