0d953bd5fee8d9a1cd492cc33ff08ec5acf66b90
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 13:52:23 +0530

    Update CWE-501.java

--- CWE-501.java
+++ CWE-501.java
@@ -1,4 +1,7 @@
 usrname = request.getParameter("usrname");
-if (session.getAttribute(ATTR_USR) == null) {
-  session.setAttribute(ATTR_USR, usrname);
+// check if user is already logged in and usrname from session is same as usrname from request
+if (session.getAttribute(ATTR_USR) != null && session.getAttribute(ATTR_USR).equals(usrname)) {
+ // do nothing
+} else {
+    session.setAttribute(ATTR_USR, usrname);
 }
