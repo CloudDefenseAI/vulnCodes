57d824acd88c3361a58be83bb52b08a179ad0d28
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-19 11:52:01 +0530

    Fixed same nonce issue

--- CWE-323.java
+++ CWE-323.java
@@ -11,12 +11,19 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class test {
     public static void main(String[] args) throws Exception {
-        String key = "SUPER_SECRET_NONCE";
+        String key = generateNonce();
         String data = "Hello World";
         String encrypted = encrypt(key, data);
         String decrypted = decrypt(key, encrypted);
         System.out.println("Encrypted: " + encrypted);
         System.out.println("Decrypted: " + decrypted);
+    }
+
+    public static String generateNonce() throws NoSuchAlgorithmException {
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[20];
+        random.nextBytes(bytes);
+        return Base64.getEncoder().encodeToString(bytes);
     }
 
     public static String encrypt(String key, String data) throws Exception {
