6cb8c6733091afb97091efd37869fc4b943493c2
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-20 12:33:50 +0530

    Create CWE-95.m

---
+++ CWE-95.m
@@ -0,0 +1,24 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:webView];
+    NSString *name = @"<script>alert('XSS');</script>";
+    NSString *html = [NSString stringWithFormat:@"<html><body><h1>Hello %@</h1></body></html>", name];
+    [webView loadHTMLString:html baseURL:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
