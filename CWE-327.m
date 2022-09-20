717eb84dacfd0e7b7132339574e0db2db0a2643f
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-20 14:36:46 +0530

    fixed hashing issue

--- CWE-327.m
+++ CWE-327.m
@@ -8,12 +8,12 @@ int main(int argc, const char * argv[]) {
         NSString *str = @"Hello World";
         const char *cstr = [str cStringUsingEncoding:NSUTF8StringEncoding];
         NSData *data = [NSData dataWithBytes:cstr length:str.length];
-        uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-        CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
-        NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
-        for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+        uint8_t digest[CC_SHA256_DIGEST_LENGTH];
+        CC_SHA256(data.bytes, (CC_LONG)data.length, digest);
+        NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+        for(int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++)
             [output appendFormat:@"%02x", digest[i]];
-        NSLog(@"SHA1 digest: %@", output);
+        NSLog(@"SHA256 digest: %@", output);
     }
     return 0;
 }
