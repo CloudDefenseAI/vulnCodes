685f3369bd52f10f4b3d27cb9f6f30d531a21c23
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-20 14:29:53 +0530

    fixed encryption issue

--- CWE-311.m
+++ CWE-311.m
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
         NSString *path = @"/tmp/test";
         NSString *content = @"test";
         [content writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        NSDictionary *attributes = @{NSFileProtectionKey: NSFileProtectionComplete};
+        [[NSFileManager defaultManager] setAttributes:attributes ofItemAtPath:path error:nil];
     }
     return 0;
 }
