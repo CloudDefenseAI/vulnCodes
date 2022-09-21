79c83be19264e06fcbcfc51933248953565fae42
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:28:57 +0530

    Create CWE-611.cs

---
+++ CWE-611.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Xml;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE foo [<!ELEMENT foo ANY ><!ENTITY xxe SYSTEM \"file:///etc/passwd\" >]><foo>&xxe;</foo>");
+            Console.WriteLine(doc.InnerText);
+        }
+    }
+}
