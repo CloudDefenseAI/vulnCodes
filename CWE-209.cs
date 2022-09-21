393223fe4b9cbbd0e5867ca1344dcbb2dc8f1d9b
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 15:48:41 +0530

    Create CWE-209.cs

---
+++ CWE-209.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string username = Request["username"];
+            string password = Request["password"];
+            string email = Request["email"];
+            // runn getUser from username, password
+            if (getUser(username, password)['email'] != email)
+            {
+                Response.Write("Invalid username or password, please try again, this email is associated with " + getUser(username, password)['email']);
+            }else{
+                Response.Write("Welcome " + username);
+            }
+    }
+}
