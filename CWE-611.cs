7513e69dfbda95c4169fe910cd38dfd0124aa870
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:29:20 +0530

    Fixed XXE

--- CWE-611.cs
+++ CWE-611.cs
@@ -8,6 +8,7 @@ namespace Test
         static void Main(string[] args)
         {
             XmlDocument doc = new XmlDocument();
+            doc.XmlResolver = null;
             doc.LoadXml("<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE foo [<!ELEMENT foo ANY ><!ENTITY xxe SYSTEM \"file:///etc/passwd\" >]><foo>&xxe;</foo>");
             Console.WriteLine(doc.InnerText);
         }
