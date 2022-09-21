4f499fe86f15c463c432953e06dbdfbddd8f9fd4
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:52:40 +0530

    prevention against SSRF

--- CWE-918.cs
+++ CWE-918.cs
@@ -11,16 +11,26 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Enter the URL to be accessed");
             string url = Console.ReadLine();
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
+            // exclusion list to prevent SSRF
+            string[] exclusionList = { "localhost", "127.0.0.1", "::1" };
+            // check if the input is in the exclusion list
+            if (exclusionList.Contains(url))
+            {
+                Console.WriteLine("Invalid URL");
+            }
+            else
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
         }
     }
 }
