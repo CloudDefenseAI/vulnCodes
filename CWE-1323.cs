9d5d21a1c69fe5ac40b6ac6ff99b6f0f6738da0d
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 15:19:39 +0530

    Create CWE-1323.cs

---
+++ CWE-1323.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Web.Configuration;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.Security;
+using System.Web.Profile;
+using System.Web.SessionState;
+using System.Web.Caching;
+using System.Web.Handlers;
+using System.Web.Hosting;
+
+public partial class test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Trace.IsEnabled = true;
+        Trace.Warn("test", "test");
+    }
+}
