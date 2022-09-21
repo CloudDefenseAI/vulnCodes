5e3512082ae2ce341049b30219657b4c9e50c9a8
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:05:16 +0530

    fixed hashing issue

--- CWE-310.cs
+++ CWE-310.cs
@@ -6,6 +6,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -20,9 +22,22 @@ public partial class _Default : System.Web.UI.Page
         string insertQuery = "insert into Table1 (username, password) values (@username, @password)";
         SqlCommand com = new SqlCommand(insertQuery, con);
         com.Parameters.AddWithValue("@username", TextBox1.Text);
-        com.Parameters.AddWithValue("@password", TextBox2.Text);
+        com.Parameters.AddWithValue("@password", GetHash(TextBox2.Text));
         com.ExecuteNonQuery();
         Response.Write("Registration is successful");
         con.Close();
+    }
+    public static string GetHash(string text)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
     }
 }
