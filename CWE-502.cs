981f904d4ed96927a5696c1bcd01acfbf39973b6
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:26:38 +0530

    Verifying the file hash

--- CWE-502.cs
+++ CWE-502.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace Test
 {
@@ -9,9 +10,29 @@ namespace Test
         static void Main(string[] args)
         {
             string path = "C:\\Users\\user\\Desktop\\test.txt";
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream fs = new FileStream(path, FileMode.Open);
-            bf.Deserialize(fs);
+            string hash = "C:\\Users\\user\\Desktop\\hash.txt";
+            string hashValue = File.ReadAllText(hash);
+            string fileHash = GetHash(path);
+            if (hashValue == fileHash)
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream fs = new FileStream(path, FileMode.Open);
+                bf.Deserialize(fs);
+            }
+            else
+            {
+                Console.WriteLine("File is modified");
+            }
+        }
+        public static string GetHash(string path)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
+                }
+            }
         }
     }
 }
