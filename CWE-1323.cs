85174fd0afef40d550ab4bcbc2e2c04eb48637b8
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 15:20:11 +0530

    fixed output trace issue

--- CWE-1323.cs
+++ CWE-1323.cs
@@ -16,7 +16,7 @@ public partial class test : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Trace.IsEnabled = true;
+        Trace.IsEnabled = false;
         Trace.Warn("test", "test");
     }
 }
