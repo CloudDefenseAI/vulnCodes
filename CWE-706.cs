abd3f395b7074be0ed249b5058383a328ccd837b
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:44:01 +0530

    fixed wildcard tenant inssue

--- CWE-706.cs
+++ CWE-706.cs
@@ -11,19 +11,26 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Enter the subdomain name");
             string tenant_name = Console.ReadLine();
-            string url = "http://" + tenant_name + ".domain.com";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            request.ContentType = "application/json";
-            request.Accept = "application/json";
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
+            if (tenant_name == "tenant1" || tenant_name == "tenant2")
+            {
+                string url = "http://" + tenant_name + ".domain.com";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.Accept = "application/json";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            else
+            {
+                Console.WriteLine("Invalid tenant name");
+            }
         }
     }
 }
