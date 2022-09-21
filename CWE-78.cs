5f479734692e0996652bca378facd90995adf623
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:46:18 +0530

    Create CWE-78.cs

---
+++ CWE-78.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Process.Start("cmd.exe", "/c echo " + name);
+        }
+    }
+}
