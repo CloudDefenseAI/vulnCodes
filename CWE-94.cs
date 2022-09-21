0e2dd63ebdfda5738e7fb0f86e759b6cdb3d23e8
Rohit Kumar <104613917+rohitcoderCdefense@users.noreply.github.com>
2022-09-21 16:57:51 +0530

    Create CWE-94.cs

---
+++ CWE-94.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string name = Request.QueryString["name"];
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=master;Integrated Security=True");
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from sysobjects where name = '" + name + "'", con);
+        SqlDataReader dr = cmd.ExecuteReader();
+        GridView1.DataSource = dr;
+        GridView1.DataBind();
+        con.Close();
+    }
+}
